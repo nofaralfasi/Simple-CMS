da7b041c7546b14596537b4ebb70b05156c1caba
nofaralfasi <nofaralfasi@gmail.com>
2022-04-17 19:39:07 +0300

    chapter 7

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,28 +1,51 @@
 class PagesController < ApplicationController
   def index
+    @pages = Page.order('position ASC')
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page = Page.new
   end
 
   def create
-
+    @page = Page.new(page_params)
+    if @page.save
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
   end
 
   def update
-
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      redirect_to(page_path(@page))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
 
   def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to(pages_path)
+  end
 
+  private
+
+  def page_params
+    params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible, :content)
   end
 end
